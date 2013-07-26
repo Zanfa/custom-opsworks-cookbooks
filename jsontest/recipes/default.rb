595fdf50f75320996f982937b89e1f4b31db6b72
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2013-07-26 17:42:41 +0200

    Added more of the AWS' own sample code

--- jsontest/recipes/default.rb
+++ jsontest/recipes/default.rb
@@ -1,9 +1,16 @@
-template "/home/ubuntu/ips" do
-  source "ips.erb"
-  mode 777
-  owner "ubuntu"
-  variables({
-    :opsworks => node[:opsworks]
-  })
-end
+# template "/home/ubuntu/ips" do
+#   source "ips.erb"
+#   mode 777
+#   owner "ubuntu"
+#   variables({
+#     :opsworks => node[:opsworks]
+#   })
+# end
   
+  
+node[:opsworks][:layers][:db-master][:instances].each do |instance_name, instance|
+ template instance_name do
+   source "ip.erb"
+   variables( :ip =>instance[:private_ip] )
+ end
+end
