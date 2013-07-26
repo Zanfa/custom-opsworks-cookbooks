e9540affee64a19e4fd1999e1d28fbf159694ce3
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2013-07-26 16:01:03 +0200

    Added json serialization

--- jsontest/recipes/default.rb
+++ jsontest/recipes/default.rb
@@ -1,9 +1,11 @@
+require 'json'
+
 template "/home/ubuntu/ips" do
   source "ips.erb"
   mode 777
   owner "ubuntu"
   variables({
-    :instances => node[:opsworks][:layers][:jsontest][:instances]
+    :json => node[:opsworks].to_json
   })
 end
   
