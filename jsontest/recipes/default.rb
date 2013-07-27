fa3799286a4547ad927f757b999f18e2d7926db1
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2013-07-27 14:27:20 +0300

    Updated db-masters layer name to jsontest

--- jsontest/recipes/default.rb
+++ jsontest/recipes/default.rb
@@ -8,7 +8,7 @@
 # end
   
   
-node[:opsworks][:layers][:db-master][:instances].each do |instance_name, instance|
+node[:opsworks][:layers][:jsontest][:instances].each do |instance_name, instance|
  template instance_name do
    source "ip.erb"
    variables( :ip =>instance[:private_ip] )
